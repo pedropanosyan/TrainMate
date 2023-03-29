27f4de740ec40580abb648f93bf9790bce6ecdda
diego <dmalvaradog26@gmail.com>
2023-03-29 11:15:28 -0300

    redirect / to home

--- train-mate/src/App.js
+++ train-mate/src/App.js
@@ -6,21 +6,25 @@ import Register from "./pages/Register";
 
 
 function App() {
-  return (
-      <Router>
-          <Switch>
-              <Route path="/login">
-                  <Login/>
-              </Route>
-              <Route path="/register">
-                  <Register/>
-              </Route>
-              <Route path="/home">
-                  <Home/>
-              </Route>
-          </Switch>
-      </Router>
-  );
+    return (
+        <Router>
+            <Switch>
+                <Route path="/login">
+                    <Login />
+                </Route>
+                <Route path="/register">
+                    <Register />
+                </Route>
+                <Route path="/home">
+                    <Home />
+                </Route>
+                { /* Redirect / to home adress*/ }
+                <Route path="/">
+                    <Home />
+                </Route>
+            </Switch>
+        </Router>
+    );
 }
 
 export default App;
