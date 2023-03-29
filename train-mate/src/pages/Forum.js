9b98789491450db8bcd22ad9d8be0d04ba9d91d9
diego <dmalvaradog26@gmail.com>
2023-03-29 12:11:49 -0300

    added Forum element

---
+++ train-mate/src/pages/Forum.js
@@ -0,0 +1,10 @@
+const divider2 = <div style={{paddingTop: "3px", backgroundColor: "#8db5ff", maxWidth: '55%', margin: "auto"}}/>
+
+function Forum() {
+    return (
+        <div style={{background:'#222222'}}>
+            Forum
+        </div>
+    )
+}
+export default Forum;
