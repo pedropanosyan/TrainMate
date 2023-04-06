7d0a687c3b39ca58bdfc5c4e2e81d840e6e5ed0f
ppanosyan <pedro.panosyan@ing.autral.edu.ar>
2023-04-06 11:01:44 -0300

    Agregue que apareza resaltada en el navbar la pagina actual en la que estamos.

--- train-mate/src/components/NavBar.js
+++ train-mate/src/components/NavBar.js
@@ -1,9 +1,10 @@
 import {Container, Nav, Navbar, Image} from 'react-bootstrap';
-import "../css/NavBar.css";
 import axios from "axios";
 
 
 function NavBar() {
+
+    const currentPath = window.location.pathname;
 
     const handleSignOut = (event) => {
         event.preventDefault();
@@ -28,18 +29,18 @@ function NavBar() {
 
     return (
         <>
-            <Navbar bg="dark" variant="dark">
+            <Navbar bg="dark" variant="dark" style={{backgroundColor:'#212529'}}>
                 <Container>
                     <Navbar.Brand className='navBar-brand' href="#home">
-                        <p>Train<span>Mate</span></p>
+                        <p style={{color: '#FFFFFF', fontSize: '1.5em'}}>Train<span style={{color:'#8db5ff'}}>Mate</span></p>
                     </Navbar.Brand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
-                        <Image src="Imagenes/logo2.png" style={{width:"90px", height:"90px"}}/>
+                        <Image src="Imagenes/logo2.png" style={{width:"80px", height:"80px"}}/>
                         <Nav className="me-auto" style = {{color: 'white', fontSize: '1.4em', marginLeft: '200px', marginTop: '-25px'}}>
-                            <Nav.Link onClick={toHome} style={{ marginRight: '70px', marginTop:'20px' }}>Home</Nav.Link>
-                            <Nav.Link onClick={toRoutine} style={{ marginRight: '70px', marginTop:'20px' }}>Routines</Nav.Link>
-                            <Nav.Link href="#forum" style={{ marginRight: '70px', marginTop:'20px'}}>Forum</Nav.Link>
+                            <Nav.Link className={`nav-link ${currentPath === '/home' ? 'active' : ''}`} onClick={toHome} style={{ marginRight: '70px', marginTop:'20px' }}>Home</Nav.Link>
+                            <Nav.Link className={`nav-link ${currentPath === '/routines' ? 'active' : ''}`} onClick={toRoutine} style={{ marginRight: '70px', marginTop:'20px' }}>Routines</Nav.Link>
+                            <Nav.Link className={`nav-link ${currentPath === '/forum' ? 'active' : ''}`} style={{ marginRight: '70px', marginTop:'20px'}}>Forum</Nav.Link>
                             <Nav.Link onClick={handleSignOut} href="/login" style= {{marginRight: '70px', marginTop:'20px'}}>Sign Out</Nav.Link>
                         </Nav>
                         </Navbar.Collapse>
