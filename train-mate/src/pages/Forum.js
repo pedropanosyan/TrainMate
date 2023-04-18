ba20d80a0a1045d0e489935e277d6fcd6ad446bc
diego <dmalvaradog26@gmail.com>
2023-04-18 20:04:56 -0300

    created forum element on pages

--- train-mate/src/pages/Forum.js
+++ train-mate/src/pages/Forum.js
@@ -1,10 +1,13 @@
+import NavBar from "../components/NavBar";
+
 const divider2 = <div style={{paddingTop: "3px", backgroundColor: "#8db5ff", maxWidth: '55%', margin: "auto"}}/>
 
 function Forum() {
     return (
         <div style={{background:'#222222'}}>
-            Forum
+        <NavBar/>
         </div>
+      
     )
 }
 export default Forum;
